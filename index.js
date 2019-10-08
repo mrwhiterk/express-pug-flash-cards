d0d7b789721e902b4f2410eaf9fad72364542a69
Ryan White <mrwhiterk@gmail.com>
2019-10-08 01:31:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', {
+    prompt: "Who is buried in Grant's tomb?",
+    hint: 'Think about whose tomb it is.'
+  });
+});
+
+app.get('/hello', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
+
+app.post('/hello', (req, res) => {
+  // cookie is used to set 'memory' between browser and server. After I set this cookie it will be sent to my server on all future request.
+
+  // note: cookies are stored in plain text - don't store sensitive data
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
+
+app.listen(3000, () => console.log('✅ : 3000'));
