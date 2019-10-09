da0f1bf23fad3946476dac027dc279e4b4ef80a3
Ryan White <mrwhiterk@gmail.com>
2019-10-08 21:23:25 -0400

    added 404 error handling & added error middleware

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
 const app = express();
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// middleware
 app.use(cookieParser());
 
+// only run middleware on /users route
+// app.get('/users', (req, res, next) => {})
+
 app.set('view engine', 'pug');
+
+// note: you can pass multiple function to one app.use
+
+/**
+ * middleware
+ */
+app.use((req, res, next) => {
+  console.log('hello');
+  // creating a custom error and passing it to next will stop the program and throw error on stack trace
+  const err = new Error('Oh noes!');
+  err.status = 500;
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('world');
+  next();
+});
 
 app.get('/', (req, res) => {
   const name = req.cookies.username;
@@ -45,6 +68,20 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
   res.clearCookie('username');
   res.redirect('/hello');
+});
+
+// if express gets this far that means not routes match and we should return 404.
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// this will catch any error that is passed to a next function, and give a templated display.
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error', err);
 });
 
 app.listen(3000, () => console.log('✅ : 3000'));
